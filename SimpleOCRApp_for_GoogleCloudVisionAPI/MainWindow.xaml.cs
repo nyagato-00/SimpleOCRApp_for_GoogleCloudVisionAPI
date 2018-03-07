70f6b4422c35d1a279f925bd6302f6ece9acdeef
nyagato_00 <nyagato.eva@gmail.com>
2018-03-07 21:08:44 +0900

    Add:Source code

---
+++ SimpleOCRApp_for_GoogleCloudVisionAPI/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+// add my class
+using GoogleCloudVisionAPI;
+
+namespace SimpleOCRApp_for_GoogleCloudVisionAPI
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage loadImage;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (image.Source != null)
+            {
+                textBox.Text = "Start OCR Process for Google Cloud Vision API";
+
+                Stream stream = loadImage.StreamSource;
+
+                byte[] imageArray = System.IO.File.ReadAllBytes(loadImage.UriSource.LocalPath);
+
+                GCPVisonAPI ocr = new GCPVisonAPI();
+
+                string getText = "";
+
+                ocr.getTextAndRoi(imageArray, ref getText);
+                textBox.Text = getText;
+            }
+            else
+            {
+                textBox.Text = "Not Load Image File....";
+            }
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Open Image File";
+            dialog.Filter = "Image Files (*.png)|*.png;";
+            if (dialog.ShowDialog() == true)
+            {
+                var source = new BitmapImage();
+                source.BeginInit();
+                source.UriSource = new Uri(dialog.FileName);
+                source.EndInit();
+                loadImage = source;
+                image.Source = source;
+
+            }
+            else
+            {
+                // this.textBlockFileName.Text = "キャンセルされました";
+            }
+        }
+    }
+}
---
+++ SimpleOCRApp_for_GoogleCloudVisionAPI/TextDetector.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Vision.v1;
+using Google.Apis.Vision.v1.Data;
+using Newtonsoft.Json;
+
+namespace GoogleCloudVisionAPI
+{
+    class GCPVisonAPI
+    {
+        private VisionService CreateAuthorizedClient()
+        {
+            GoogleCredential credential =
+                GoogleCredential.GetApplicationDefaultAsync().Result;
+            // Inject the Cloud Vision scopes
+            if (credential.IsCreateScopedRequired)
+            {
+                credential = credential.CreateScoped(new[]
+                {
+                    VisionService.Scope.CloudPlatform
+                });
+            }
+            return new VisionService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                GZipEnabled = false
+            });
+        }
+
+        private int DetectTextWord(
+            VisionService vision, byte[] getImage, ref string FullText)
+        {
+            int result = 1;
+            Console.WriteLine("Detecting image to texts...");
+            // Convert image to Base64 encoded for JSON ASCII text based request
+            string imageContent = Convert.ToBase64String(getImage);
+
+            try
+            {
+                // Post label detection request to the Vision API
+                var responses = vision.Images.Annotate(
+                    new BatchAnnotateImagesRequest()
+                    {
+                        Requests = new[] {
+                    new AnnotateImageRequest() {
+                        Features = new [] { new Feature() { Type =
+                          "TEXT_DETECTION"}},
+                        Image = new Image() { Content = imageContent }
+                    }
+                   }
+                    }).Execute();
+
+                if (responses.Responses != null)
+                {
+                    FullText = responses.Responses[0].TextAnnotations[0].Description;
+
+                    Console.WriteLine("SUCCESS：Cloud Vision API Access.");
+                    result = 0;
+                }
+                else
+                {
+                    FullText = "";
+                    Console.WriteLine("ERROR : No text found.");
+                    result = -1;
+                }
+            }
+            catch
+            {
+                FullText = "";
+                Console.WriteLine("ERROR : Not Access Cloud Vision API.");
+                result = -1;
+            }
+
+            return result;
+        }
+
+        public int getTextAndRoi(byte[] getImage, ref string FullText)
+        {
+            int result = 1;
+            GCPVisonAPI sample = new GCPVisonAPI();
+            // Create a new Cloud Vision clieuthorint azed via Application
+            // Default Credentials
+            VisionService vision = sample.CreateAuthorizedClient();
+            // Use the client to get label annotations for the given image
+            string getFullText = "";
+            result = sample.DetectTextWord(vision, getImage, ref getFullText);
+            FullText = getFullText;
+            return result;
+        }
+    }
+}
